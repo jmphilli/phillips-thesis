7b5ca9bf0d49267291180e7a3310b6b0cad83a3f
Justin Phillips <jjustinphillipss@gmail.com>
2010-12-02 17:47:25 -0800

    just-play added

--- Driver.rkt
+++ Driver.rkt
@@ -15,7 +15,6 @@
 TODO:
 
 remove 'NC's if i can
-just playing function..
 I think that the modulo call below is unneccessary, should just be length of music i think... 
 
 try '3:4
@@ -26,30 +25,6 @@ try '3:4
 (define tempo (box 'a))
 (define clock (- seconds (value-now seconds)))
 
-(define generator (event-receiver))
-(set-box! tempo 120)
-(init-performer music-value-function clock (unbox tempo))
-(define my-piece (box '(:+: )))
-
-(if (equal? 1 (modulo seconds 2))
-    (begin
-      (set-box! my-piece (append (unbox my-piece) '((:=: (note (C 3) 1) (note (E 3) 1) (note (G 3) 1)))))
-      (send-event generator (unbox my-piece)))
-    (begin
-      (set-box! my-piece (append (unbox my-piece) '((:=: (note (G 3) 1) (note (B 3) 1) (note (D 3) 1)))))
-      (send-event generator (unbox my-piece))))
-
-(map-e
- (lambda (pkt) 
-   (begin
-     (set-box! analysed-piece (analyse pkt (unbox analysed-piece)))
-     (let ([music (perform (unbox analysed-piece))])
-       (cond [(not (empty? music)) (update-music-signal (piece-key-signature (unbox analysed-piece)) music (modulo (length (piece-changes (unbox analysed-piece))) (length music)))]))
-     )
-   )
- generator)
-
-#|
 (define scheme-to-frtime-evt (event-receiver))
 
 (define (let-midi-flow)
@@ -77,4 +52,30 @@ try '3:4
      )
    )
  scheme-to-frtime-evt)
-|#
+
+(define (just-play-music)
+  (begin
+    (define generator (event-receiver))
+    (set-box! tempo 120)
+    (init-performer music-value-function clock (unbox tempo))
+    (define my-piece (box '(:+: )))
+
+    (if (equal? 1 (modulo seconds 2))
+        (begin
+          (set-box! my-piece (append (unbox my-piece) '((:=: (note (C 3) 1) (note (E 3) 1) (note (G 3) 1)))))
+          (send-event generator (unbox my-piece)))
+        (begin
+          (set-box! my-piece (append (unbox my-piece) '((:=: (note (G 3) 1) (note (B 3) 1) (note (D 3) 1)))))
+          (send-event generator (unbox my-piece))))
+
+    (map-e
+     (lambda (pkt) 
+       (begin
+         (set-box! analysed-piece (analyse pkt (unbox analysed-piece)))
+         (let ([music (perform (unbox analysed-piece))])
+           (cond [(not (empty? music)) (update-music-signal (piece-key-signature (unbox analysed-piece)) music (modulo (length (piece-changes (unbox analysed-piece))) (length music)))]))
+         )
+       )
+     generator)
+    )
+  )
